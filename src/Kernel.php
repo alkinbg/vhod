996817efb48d27682e8569173c2e5f837d0164c4
Alkin Fehim <alkinbg@gmail.com>
2026-09-07 23:08:38 +0300

    build: bootstrap Symfony 8.1 application

---
+++ src/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+final class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * @return list<string>
+     */
+    private function getAllowedEnvs(): array
+    {
+        return ['prod', 'dev', 'test'];
+    }
+}
