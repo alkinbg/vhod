31de6f1d2208e445c989a77f7b5625fc5d28d29f
Alkin Fehim <alkinbg@gmail.com>
2026-09-08 13:14:20 +0300

    test: cover stable bank transaction identity

--- tests/Value/BankTransactionFingerprintTest.php
+++ tests/Value/BankTransactionFingerprintTest.php
@@ -10,57 +10,85 @@ use PHPUnit\Framework\TestCase;
 
 final class BankTransactionFingerprintTest extends TestCase
 {
-    public function testFingerprintIsDeterministicAcrossSemanticWhitespaceAndIbanCase(): void
+    public function testAccountServicerReferenceProvidesStableIdentityAcrossChangingOptionalMetadata(): void
     {
         $first = BankTransactionFingerprint::fromFields(
-            'BG80BNBG96611020345678',
+            'BG79TEST00000100000000',
             12550,
             new DateTimeImmutable('2026-09-08'),
             new DateTimeImmutable('2026-09-09'),
-            ' TX-1 ',
-            ' ENTRY-1 ',
-            ' E2E-1 ',
-            ' bg40 bnbg 9661 1000 0661 23 ',
-            '  Такса   ап. 12  ',
+            'TX-OLD',
+            ' ASR-STABLE-1 ',
+            'E2E-OLD',
+            'BG88FAKE00000200000001',
+            'Такса ап. 12',
         );
         $second = BankTransactionFingerprint::fromFields(
-            ' bg80 bnbg 9661 1020 3456 78 ',
+            ' bg79 test 0000 0100 0000 00 ',
             12550,
             new DateTimeImmutable('2026-09-08 19:30:00 UTC'),
-            new DateTimeImmutable('2026-09-09 01:00:00 UTC'),
-            'TX-1',
-            'ENTRY-1',
-            'E2E-1',
-            'BG40BNBG96611000066123',
-            'Такса ап. 12',
+            null,
+            'TX-NEW',
+            'asr-stable-1',
+            'E2E-NEW',
+            null,
+            'Променено описание от банката',
         );
 
         self::assertSame($first, $second);
         self::assertMatchesRegularExpression('/^[a-f0-9]{64}$/', $first);
     }
 
-    public function testMaterialDifferenceChangesFingerprint(): void
+    public function testFallbackIgnoresUnstableOptionalReferencesWhenAccountServicerReferenceIsMissing(): void
     {
         $first = BankTransactionFingerprint::fromFields(
-            'BG80BNBG96611020345678',
+            'BG79TEST00000100000000',
+            12550,
+            new DateTimeImmutable('2026-09-08'),
+            new DateTimeImmutable('2026-09-09'),
+            'TX-OLD',
+            null,
+            'E2E-OLD',
+            ' bg88 fake 0000 0200 0000 01 ',
+            '  Такса   ап. 12  ',
+        );
+        $second = BankTransactionFingerprint::fromFields(
+            'BG79TEST00000100000000',
+            12550,
+            new DateTimeImmutable('2026-09-08 21:00:00 UTC'),
+            null,
+            'TX-NEW',
+            null,
+            'E2E-NEW',
+            'BG88FAKE00000200000001',
+            'Такса ап. 12',
+        );
+
+        self::assertSame($first, $second);
+    }
+
+    public function testMaterialFallbackDifferenceChangesFingerprint(): void
+    {
+        $first = BankTransactionFingerprint::fromFields(
+            'BG79TEST00000100000000',
             12550,
             new DateTimeImmutable('2026-09-08'),
             null,
             null,
-            'ENTRY-1',
             null,
             null,
+            'BG88FAKE00000200000001',
             'Такса ап. 12',
         );
         $second = BankTransactionFingerprint::fromFields(
-            'BG80BNBG96611020345678',
+            'BG79TEST00000100000000',
             12551,
             new DateTimeImmutable('2026-09-08'),
             null,
             null,
-            'ENTRY-1',
             null,
             null,
+            'BG88FAKE00000200000001',
             'Такса ап. 12',
         );
 
