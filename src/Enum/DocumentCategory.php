a5f52b741679c8ac7f5dcda10b6d717da2d036ee
Alkin Fehim <alkinbg@gmail.com>
2026-09-09 09:28:19 +0300

    feat: add document categories

---
+++ src/Enum/DocumentCategory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum DocumentCategory: string
+{
+    case MEETING_INVITATION = 'meeting_invitation';
+    case MEETING_MINUTES = 'meeting_minutes';
+    case HOUSE_RULES = 'house_rules';
+    case INVOICE_RECEIPT = 'invoice_receipt';
+    case CONTRACT_OFFER = 'contract_offer';
+    case WARRANTY = 'warranty';
+    case BANK_STATEMENT = 'bank_statement';
+    case TECHNICAL_DOCUMENTATION = 'technical_documentation';
+    case MONTHLY_REPORT = 'monthly_report';
+    case OTHER = 'other';
+
+    public function labelBg(): string
+    {
+        return match ($this) {
+            self::MEETING_INVITATION => 'Покана за общо събрание',
+            self::MEETING_MINUTES => 'Протокол от общо събрание',
+            self::HOUSE_RULES => 'Правилник за вътрешния ред',
+            self::INVOICE_RECEIPT => 'Фактура / касов документ',
+            self::CONTRACT_OFFER => 'Договор / оферта',
+            self::WARRANTY => 'Гаранция',
+            self::BANK_STATEMENT => 'Банково извлечение',
+            self::TECHNICAL_DOCUMENTATION => 'Техническа документация',
+            self::MONTHLY_REPORT => 'Месечен отчет',
+            self::OTHER => 'Друг документ',
+        };
+    }
+}
